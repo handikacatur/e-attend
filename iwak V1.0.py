40edbbee05954c58c064e86bd89a897f935b454b
Handika Catur Mahardika <55946389+handikacatur@users.noreply.github.com>
2020-04-06 12:52:41 +0700

    adding toaster

--- iwak V1.0.py
+++ iwak V1.0.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import platform
+import win10toast
 from getpass import getpass
 import datetime
 import time
@@ -12,6 +13,10 @@ password = ''
 links = []
 sso = 'https://sso.unej.ac.id/cas/login?service=https%3A%2F%2Fmmp.unej.ac.id%2Flogin%2Findex.php'
 driver = None
+
+def toaster(msg):
+    toast = win10toast.ToastNotifier()
+    toast.show_toast('E-Attendance', str(msg))
 
 def initChrome():
     global driver
@@ -55,15 +60,22 @@ def loggingIn():
             if 'Dashboard' in getTitle():
                 name = driver.find_element_by_class_name('usermenu').find_element_by_xpath('./ul/li/a').text
                 print('\n>>> %s: BERHASIL LOGIN <<<' %name)
+                toaster('%s BERHASIL LOGIN' %name)
                 logged_in = True
             else:
                 print('\n!!! LOGIN GAGAL !!!')
+                toaster('!!! LOGIN GAGAL !!!')
                 if 'Bad Gateway' in getTitle():
                     print('\nServer sedang error.')
+                    toaster('Server error')
                 else:
                     print('\nUsername atau password salah.')
+                    toaster('Username atau password salah')
                 print('\nProgram dihentikan...')
+                driver.close()
                 break
+        elif 'Bad Gateway' in getTitle():
+            toaster('Bad Gateway')
         else:
             print('>>> Gagal login. Mencoba login kembali')
             driver.refresh()
@@ -80,10 +92,15 @@ def getEvents():
         if i.find_element_by_class_name('date').find_element_by_xpath('./a').text == 'Today':
             links.append(i.find_element_by_xpath('./a').get_attribute('href'))
 
+    toaster('Event hari ini berhasil diambil')
+
 def status_matkul(status):
+    matkul = driver.find_element_by_class_name('page-header-headings').find_element_by_xpath('./h1').text
+
     print('\n\n>>> ', end='')
-    print(driver.find_element_by_class_name('page-header-headings').find_element_by_xpath('./h1').text, end=' : ')
+    print(matkul, end=' : ')
     print(status)
+    toaster('%s : %s' %(matkul, status))
 
 def absenin():
     driver.find_elements_by_class_name('statuscol')[1].find_element_by_xpath('./a').click()
@@ -100,8 +117,10 @@ def getStatus():
                 status_matkul('Sudah Absen!')
             else:
                 print('\n>> Error absen dari mmp. Coba lu hubungin ketua kelas atau dosennya.')
+                toaster('Error absen dari mmp. Coba lu hubungin ketua kelas atau dosennya.')
         else:
             print('\n>> Error absen dari mmp. Coba lu hubungin ketua kelas atau dosennya.')
+            toaster('Error absen dari mmp. Coba lu hubungin ketua kelas atau dosennya.')
 
 def checkAttendance():
     global links
@@ -131,6 +150,7 @@ def checkAttendance():
         if (target-now).total_seconds() > 0:
             status_matkul('Absen dimulai pada ' + str(target))
             print('\nSantuy ya, ntar pas udah lebih 4 menit gw absenin')
+            toaster('Santuy ya, ntar pas udah lebih 4 menit gw absenin')
 
             time.sleep((target-now).total_seconds() + 240)
 
@@ -168,4 +188,6 @@ checkAttendance()
 # end
 print('\n>>> Udah ga ada absen buat hari ini. Jangan lupa besok jalanin gw lagi ya. Selamat bersantuy...')
 print('\n\n>>> Program dihentikan <<<')
+toaster('Udah ga ada absen buat hari ini. Jangan lupa besok jalanin gw lagi ya. Selamat bersantuy...')
+toaster('PROGRAM DIHENTIKAN')
 driver.close()
