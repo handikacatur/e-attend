e3d222e2bf389d643b7ba0419baf5e2824b0f7c5
Handika Catur Mahardika <55946389+handikacatur@users.noreply.github.com>
2020-04-06 12:34:35 +0700

    improve

---
+++ iwak V1.0.py
@@ -0,0 +1,171 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import platform
+from getpass import getpass
+import datetime
+import time
+
+print('\n>>> E-ATTEND UNTUK E-LEARNING v1.0 (beta) <<<')
+
+username = ''
+password = ''
+links = []
+sso = 'https://sso.unej.ac.id/cas/login?service=https%3A%2F%2Fmmp.unej.ac.id%2Flogin%2Findex.php'
+driver = None
+
+def initChrome():
+    global driver
+
+    # initiating chrome options
+    chrome_options = Options()
+    # chrome_options.add_argument('--disable-gpu')
+    # chrome_options.add_argument('--headless')
+
+    # initiate headless chrome
+    if 'Windows' in platform.system():
+        driver = webdriver.Chrome('./webdrivers/chromedriver.exe', options=chrome_options)
+    elif 'Linux' in platform.system():
+        chrome_options.add_argument('--no-sandbox')
+        driver = webdriver.Chrome('./webdrivers/chromedriver', options=chrome_options)
+
+def getUserInfo():
+    global username
+    global password
+    username = input('Username: ')
+    password = getpass()
+
+def doLogin():
+    # getting the username and password forms
+    username_form = driver.find_element_by_id('username')
+    password_form = driver.find_element_by_id('password')
+
+    # sending keys to username and password forms
+    username_form.send_keys(username)
+    password_form.send_keys(password)
+
+    # click the login button
+    driver.find_element_by_name('submit').click()
+
+def loggingIn():
+    logged_in = False
+    
+    while logged_in == False:
+        if 'Single Sign On' in getTitle():
+            doLogin()
+            if 'Dashboard' in getTitle():
+                name = driver.find_element_by_class_name('usermenu').find_element_by_xpath('./ul/li/a').text
+                print('\n>>> %s: BERHASIL LOGIN <<<' %name)
+                logged_in = True
+            else:
+                print('\n!!! LOGIN GAGAL !!!')
+                if 'Bad Gateway' in getTitle():
+                    print('\nServer sedang error.')
+                else:
+                    print('\nUsername atau password salah.')
+                print('\nProgram dihentikan...')
+                break
+        else:
+            print('>>> Gagal login. Mencoba login kembali')
+            driver.refresh()
+    
+
+def getTitle():
+    return driver.title
+
+def getEvents():
+    global links
+    events = driver.find_elements_by_class_name('event')
+
+    for i in events:
+        if i.find_element_by_class_name('date').find_element_by_xpath('./a').text == 'Today':
+            links.append(i.find_element_by_xpath('./a').get_attribute('href'))
+
+def status_matkul(status):
+    print('\n\n>>> ', end='')
+    print(driver.find_element_by_class_name('page-header-headings').find_element_by_xpath('./h1').text, end=' : ')
+    print(status)
+
+def absenin():
+    driver.find_elements_by_class_name('statuscol')[1].find_element_by_xpath('./a').click()
+    driver.find_element_by_xpath('//input[@name="status"][1]').click()
+    driver.find_element_by_xpath('//input[@name="submitbutton"]').click()
+
+def getStatus():
+    if driver.find_elements_by_class_name('statuscol')[1].text == 'Present':
+        status_matkul('Sudah Absen!')
+    else:
+        if driver.find_elements_by_class_name('statuscol')[1].find_element_by_xpath('./a').text == 'Submit attendance':
+            absenin()
+            if driver.find_elements_by_class_name('statuscol')[1].text == 'Present':
+                status_matkul('Sudah Absen!')
+            else:
+                print('\n>> Error absen dari mmp. Coba lu hubungin ketua kelas atau dosennya.')
+        else:
+            print('\n>> Error absen dari mmp. Coba lu hubungin ketua kelas atau dosennya.')
+
+def checkAttendance():
+    global links
+    hour = 0
+    minutes = 0
+    error = True
+
+    for i in links:
+        driver.get(i)
+        
+        if ':' in driver.find_element_by_class_name('date').text[7:9]:
+            if 'PM' in driver.find_element_by_class_name('date').text[12:14]:
+                hour = int('0' + driver.find_element_by_class_name('date').text[7]) + 12
+                minutes = int(driver.find_element_by_class_name('date').text[9:11])
+            else:
+                hour = int('0' + driver.find_element_by_class_name('date').text[7])
+                minutes = int(driver.find_element_by_class_name('date').text[9:11])
+        else:
+            hour = int(driver.find_element_by_class_name('date').text[7:9])
+            minutes = int(driver.find_element_by_class_name('date').text[10:12])
+
+        target = datetime.datetime.combine(datetime.date.today(), datetime.time(hour = hour, minute = minutes))
+        now = datetime.datetime.now()
+
+        driver.find_element_by_class_name('calendar_event_attendance').find_element_by_xpath('./a').click()
+
+        if (target-now).total_seconds() > 0:
+            status_matkul('Absen dimulai pada ' + str(target))
+            print('\nSantuy ya, ntar pas udah lebih 4 menit gw absenin')
+
+            time.sleep((target-now).total_seconds() + 240)
+
+            driver.refresh()
+
+            while error == True:
+                if 'Sign On' in getTitle():
+                    doLogin()
+                else:
+                    getStatus()
+                    error = False
+        else:
+            getStatus()
+                
+    
+
+# getting user info
+getUserInfo()
+
+# initiate the driver
+initChrome()
+
+# opening sso login
+driver.get(sso)
+
+# logging in
+loggingIn()
+
+# getting events
+getEvents()
+
+# checking attendance in events
+checkAttendance()
+
+# end
+print('\n>>> Udah ga ada absen buat hari ini. Jangan lupa besok jalanin gw lagi ya. Selamat bersantuy...')
+print('\n\n>>> Program dihentikan <<<')
+driver.close()
